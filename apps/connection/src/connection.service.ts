68578244db1fecb72223cdb9abe1101c0cef625b
Ankita Patidar <ankita.patidar@ayanworks.com>
2025-08-28 17:08:40 +0530

    refactor:get connection by orgID for selecting invitationDID
    Signed-off-by: Ankita Patidar <ankita.patidar@ayanworks.com>

--- apps/connection/src/connection.service.ts
+++ apps/connection/src/connection.service.ts
@@ -650,17 +650,13 @@ export class ConnectionService {
         throw new NotFoundException(ResponseMessages.connection.error.agentEndPointNotFound);
       }
 
-      let legacyinvitationDid;
+      let legacyInvitationDid;
       if (IsReuseConnection) {
-        const data: agent_invitations[] = await this.connectionRepository.getInvitationDidByOrgId(orgId);
-           if (data && 0 < data.length) {
-            const [firstElement] = data;
-            legacyinvitationDid = firstElement?.invitationDid ?? undefined;
-            
-            this.logger.log('legacyinvitationDid:', legacyinvitationDid);
-        }
+        const invitation: agent_invitations = await this.connectionRepository.getInvitationDidByOrgId(orgId);
+        legacyInvitationDid = invitation?.invitationDid ?? undefined;
+        this.logger.debug('legacyInvitationDid:', legacyInvitationDid);
       }
-      const connectionInvitationDid = invitationDid ? invitationDid : legacyinvitationDid;
+      const connectionInvitationDid = invitationDid ? invitationDid : legacyInvitationDid;
 
       this.logger.log('connectionInvitationDid:', connectionInvitationDid);
 
